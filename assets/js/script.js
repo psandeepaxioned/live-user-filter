ac78d22eb32f0fa99179c5407639bcb02204d8c6
psandeepaxioned <vikasb@axioned.com>
2022-07-07 17:27:32 +0530

    working on the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,73 @@
 /* Author: 
 
 */
+const users = document.querySelector('ul')
+const filter = document.querySelector('.filter')
+// const userList = []
+
+getUsers()
+
+function getUsers() {
+  fetch(`https://randomuser.me/api?results=20`)
+    .then((res) => res.json())
+    .then((data) => {
+			console.log(data);
+
+			data.results.forEach(user => {
+				console.log(user)
+
+				const li = document.createElement('li');
+				const image = user.picture.medium;
+				const firstName = user.name.first;
+				const lastName = user.name.last;
+				const state = user.location.state;
+				const country = user.location.country;
+
+				// userList.push(li)
+
+				const usersInnerHTML = `
+					<figure><img src="${image}" alt="User Profile"></figure>
+					<div class="info">
+				    <span class="name">${firstName} ${lastName}</span>
+				    <span class="location">${state}, ${country}</span>
+				  </div>
+				`;
+
+			li.innerHTML = usersInnerHTML;
+			users.appendChild(li);
+
+			});
+  });
+}
+
+// function fetchUsers(){
+// 		for(let i=1; i<=20;i++){
+// 			getUsers(i);
+// 		}
+// }
+
+// fetchusers()
+
+//  function createUsers(user) {
+// 	const li = document.createElement('li');
+// 	const image = user.picture.medium;
+// 	const firstName = user.name.first;
+// 	const lastName = user.name.last;
+// 	const state = user.location.state;
+// 	const country = user.location.country;
 
 
+// 	const usersInnerHTML = `
+// 	<figure><img src="${image}" alt="User Profile"></figure>
+// 	<div class="info">
+//     <span class="name">${firstName} ${lastName}</span>
+//     <span class="location">${state} ${country}</span>
+//   </div>
+// 	`;
 
+// 	li.innerHTML = usersInnerHTML;
+// 	users.appendChild(li);
+// }
 
 
 
